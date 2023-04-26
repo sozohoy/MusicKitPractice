d5fe2859b91aa90aba8039007d3ae2aa259b7204
sozohoy <sozohoy@gmail.com>
2023-04-26 15:54:16 +0900

    project init

---
+++ MusicKitPractice/MusicKitPractice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MusicKitPractice
+//
+//  Created by 한지석 on 2023/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicKitPractice/MusicKitPractice/MusicKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKitPracticeApp.swift
+//  MusicKitPractice
+//
+//  Created by 한지석 on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
